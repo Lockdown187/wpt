059e6088c82cb928d29d58efbe2ca8c68ab88d7d
Mike Wasserman <msw@chromium.org>
2023-06-02 17:18:05 -0700

    Rewrite multi-screen window placement WPTs
    Change-Id: I97b690407fdf8f292efff179fdda6cd680a65931

--- window-placement/resources/helpers.js
+++ window-placement/resources/helpers.js
@@ -1,27 +1,15 @@
-
-// Logs (appends) an HTML string to a logger element in a list format.
-// An element in the document with id "logger" will be used as the log
-// container.
-function log(str) {
-  const entry = document.createElement('li');
-  entry.innerHTML = str;
-  const loggerElement = document.getElementById('logger');
-  loggerElement.appendChild(entry);
-  return entry;
+// Appends a list item with `innerHTML` to the document's 'list' element.
+function log(innerHTML) {
+  const li = document.createElement('li');
+  li.innerHTML = innerHTML;
+  document.getElementById('list').appendChild(li);
 }
 
-// Common setup for window management tests. Performs some basic assertions, and
-// then waits for a click on the `setUpButton` element (for manual tests).
-// Example usage:
-//  promise_test(async setUpTest => {
-//    await setUpWindowManagement(setUpTest, setUpButton);
-//    ...
-//  });
-async function setUpWindowManagement(setUpTest, setUpButton) {
+// Grants `window-management` permission and caches `window.screenDetails`.
+async function setUpWindowManagement(test) {
   assert_true(
     'getScreenDetails' in self && 'isExtended' in screen,
-    `API not supported; use Chromium (not content_shell) and enable
-     chrome://flags/#enable-experimental-web-platform-features`);
+    `API not supported; use Chromium (not content_shell)`);
   if (!screen.isExtended)
     log(`WARNING: Use multiple screens for full test coverage`);
   if (window.location.href.startsWith('file'))
@@ -31,38 +19,85 @@ async function setUpWindowManagement(setUpTest, setUpButton) {
     await test_driver.set_permission({ name: 'window-management' }, 'granted');
   } catch {
   }
-  const setUpWatcher = new EventWatcher(setUpTest, setUpButton, ['click']);
-  const setUpClick = setUpWatcher.wait_for('click');
-  try {  // Support manual testing where test_driver is not running.
-    await test_driver.click(setUpButton);
-  } catch {
-  }
-  await setUpClick;
-  setUpButton.disabled = true;
+  await buttonClick(test, 'Request screen details');
+  window.screenDetails = await window.getScreenDetails();
+  assert_true(!!window.screenDetails, 'Error getting screen details');
+
+  // TODO(msw@chromium.org): Resolve delayed currentScreen init when running:
+  // ./wpt run chrome window-placement/multi-screen-* 
+  let currentScreen = window.screenDetails.currentScreen;
+  log(`Window ${windowLog(window)} on screen ${screenLog(currentScreen)}`);
+  await new Promise(r => setTimeout(r, 1000));
+  currentScreen = window.screenDetails.currentScreen;
+  log(`Window ${windowLog(window)} on screen ${screenLog(currentScreen)}`);
 }
 
-
-// Adds a button to the given `buttonContainer` element with the contents of
-// `name`. Attaches an event watcher to the given test and waits for a signal
-// from the test driver to click the button. If no test driver is available
-// (manual testing) then awaits an actual click from the user instead. If
-// `disableOnClick` is true, the button will also be disabled after it is
-// clicked.
-async function addTestTriggerButtonAndAwaitClick(buttonContainer, name, test) {
+// Appends a button with `innerHTML` to the document's `list` element.
+// Waits for a test driver or manual click, and disables the button afterwards.
+async function buttonClick(test, innerHTML) {
   const button = document.createElement('button');
-  button.innerHTML = name;
-  const entry = document.createElement('li');
-  entry.appendChild(button);
-  buttonContainer.appendChild(entry);
-  const testWatcher = new EventWatcher(test, button, ['click']);
-  const buttonClick = testWatcher.wait_for('click');
-  // Disable the button when it is clicked.
-  button.onclick = function() {
-    button.disabled = true;
-  };
+  button.innerHTML = innerHTML;
+  const li = document.createElement('li');
+  li.appendChild(button)
+  document.getElementById('list').appendChild(li);
+  const click = new EventWatcher(test, button, ['click']).wait_for('click');
   try {  // Support manual testing where test_driver is not running.
     await test_driver.click(button);
   } catch {
   }
-  await buttonClick;
+  await click;
+  button.disabled = true;
+}
+
+// Returns true if window `w` bounds are on screen `s` with error `e`.
+function isWindowOnScreen(w, s, e = 100) {
+  log(`isWindowOnScreen w: ${windowLog(w)} s: ${screenLog(s)}`);                        // MSW 
+  return (w.screenLeft >= s.left - e) && (w.screenTop >= s.top - e) &&
+          (w.screenLeft + w.outerWidth <= s.left + s.width + e) &&
+          (w.screenTop + w.outerHeight <= s.top + s.height + e);
+}
+
+// Polls until `condition` is true, with the given `interval` and `duration`.
+// Returns a promise that will be resolved on success or timeout.
+// TODO(msw@chromium.org): Resolve incorrect originalScreen in multi-screen-fullscreen, when running via: 
+// ./wpt run chrome window-placement/multi-screen-* 
+async function poll(condition, interval = 100, duration = 30000) { //3000) {        // MSW
+  const timeout = Date.now() + duration;
+  const loop = async (resolve, reject) => {
+    if (condition() || Date.now() > timeout)
+      resolve();
+    else
+      setTimeout(loop, interval, resolve, reject);
+  }
+  return new Promise(loop);
+}
+
+// Asserts window `w` bounds and currentScreen match screen `s`.
+// Awaits pending changes, and permits small bounds errors.
+// Fullscreen promises may resolve before bounds change; e.g. crbug.com/1330724.
+async function assertWindowOnScreen(w, s) {
+  log(`assertWindowOnScreen w: ${windowLog(w)} s: ${screenLog(s)}`);                         // MSW 
+  // if (w.screenDetails.currentScreen != s) {
+  //   await new Promise(r => w.screenDetails.addEventListener(
+  //     'currentscreenchange',
+  //     () => {
+  //       if (w.screenDetails.currentScreen == s)
+  //         r();
+  //     }));
+  // }
+  await poll(() => { return s === w.screenDetails.currentScreen; });
+  assert_equals(screenLog(s), screenLog(w.screenDetails.currentScreen));
+
+  await poll(() => { return isWindowOnScreen(w, s); });
+  assert_true(isWindowOnScreen(w, s), `${windowLog(w)} on ${screenLog(s)}`);
+}
+
+// Returns a string with the label and bounds of screen `s` for logging.
+function screenLog(s) {
+  return `'${s.label}': (${s.left},${s.top} ${s.width}x${s.height})`;
+}
+
+// Returns a string with the bounds of window `w` for logging.
+function windowLog(w) {
+  return `(${w.screenLeft},${w.screenTop} ${w.outerWidth}x${w.outerHeight})`;
 }
